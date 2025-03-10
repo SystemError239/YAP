bb0ebc73e7f630302a4b59c086a888c2d34d7261
SystemError239 <154071308+SystemError239@users.noreply.github.com>
2025-03-10 19:27:56 +0500

    Update lab3.fs

--- lab3.fs
+++ lab3.fs
@@ -65,12 +65,13 @@ let readStringsFromConsoleLazy count =
         }
     )
 
-// чтение файлов в каталоге и поиск первого по алфавиту
+// чтение файлов в каталоге и поиск первого по алфавиту 
 let AlphaSearch (directoryPath: string) =
     try
         Directory.GetFiles(directoryPath)
         |> Seq.ofArray
         |> Seq.map Path.GetFileName
+        |> Seq.filter (fun file -> file.Length > 0 && Char.IsLetter(file.[0]))
         |> Seq.sortBy (fun file -> file.ToLower())
         |> Seq.tryHead
     with
@@ -146,7 +147,7 @@ and task3Menu () =
     printf "Введите путь к каталогу: "
     let directoryPath = Console.ReadLine().Trim()
     match AlphaSearch directoryPath with
-    | Some file -> printfn "Первый по алфавиту файл: %s" (Path.GetFileName(file))
+    | Some file -> printfn "Первый по алфавиту файл : %s" (Path.GetFileName(file))
     | None -> printfn "Файл не найден или произошла ошибка."
     main()
 
